c23fadc527600b62068c53348dae7db329155e9c
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-02-08 15:45:36 +0300

    draft landing page

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,4 +1,4 @@
 # step1
-from flask import Blueprint
-main = Blueprint('main', __name__)
-from . import views, errors
+# from flask import Blueprint
+# main = Blueprint('main', __name__)
+# from . import views, errors
